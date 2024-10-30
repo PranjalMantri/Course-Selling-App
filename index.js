6e3678d890080154733ed8eb97522fa5bb26a477
Pranjal Mantri <pranjalmantri@example.com>
2024-10-30 22:57:42 +0530

    Add userRouter and courseRouter

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import { userRouter } from "./routes/user.router.js";
+import { courseRouter } from "./routes/course.router.js";
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
-
-app.post("/signup", (req, res) => {});
-
-app.post("/signin", (req, res) => {});
-
-app.post("/course/purchase", (req, res) => {});
-
-app.get("/courses", (req, res) => {});
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
 
 app.listen(port, () => {
   console.log("Server listening on port 3000");
---
+++ routes/course.router.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const courseRouter = Router();
+
+courseRouter.get("/", (req, res) => {});
+courseRouter.post("/purchase", (req, res) => {});
+
+export { courseRouter };
